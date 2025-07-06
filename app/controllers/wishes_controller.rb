7dd35bcdfdc84e00a9af1facc0471dab7e6d2cf7
amami <amami_gt0403@yahoo.co.jp>
2025-07-06 10:56:26 +0900

    improve: Wish#index クラス名を持たせたオブジェクトの配列を返すように変更

--- app/controllers/wishes_controller.rb
+++ app/controllers/wishes_controller.rb
@@ -1,7 +1,10 @@
 class WishesController < ApplicationController
   def index
     @wish = Wish.new
-    @wishes = Wish.order(created_at: :desc)
+    @wishes = Wish.order(created_at: :desc).map.with_index do |wish, i|
+      color = %w[bg-pink-300 bg-yellow-300 bg-blue-300 bg-green-300 bg-purple-300][i % 5]
+      OpenStruct.new(wish: wish, color: color)
+    end
   end
 
   def create
