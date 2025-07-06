ed38d52be98ff2e6f1738c18fba5a978639be436
amami <amami_gt0403@yahoo.co.jp>
2025-07-06 10:06:16 +0900

    add: Wish#index, #create を追加

--- app/controllers/wishes_controller.rb
+++ app/controllers/wishes_controller.rb
@@ -1,2 +1,22 @@
 class WishesController < ApplicationController
+  def index
+    @wish = Wish.new
+    @wishes = Wish.order(created_at: :desc)
+  end
+
+  def create
+    @wish = Wish.new(wish_params)
+    if @wish.save
+      redirect_to root_path, notice: "願いごとを投稿しました"
+    else
+      @wishes = Wish.order(created_at: :desc)
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def wish_params
+    params.require(:wish).permit(:name, :message)
+  end
 end
