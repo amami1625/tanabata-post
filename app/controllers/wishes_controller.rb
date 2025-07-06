512e4f6781121b2608dccaf707239dbefa34c5c3
amami <amami_gt0403@yahoo.co.jp>
2025-07-06 12:06:13 +0900

    fix: TailwindCSS で動的なクラス当てがうまくいかなかったので取得するデータの内容を元に戻した

--- app/controllers/wishes_controller.rb
+++ app/controllers/wishes_controller.rb
@@ -1,18 +1,15 @@
 class WishesController < ApplicationController
+  before_action :set_wishes
+
   def index
     @wish = Wish.new
-    @wishes = Wish.order(created_at: :desc).map.with_index do |wish, i|
-      color = %w[bg-pink-300 bg-yellow-300 bg-blue-300 bg-green-300 bg-purple-300][i % 5]
-      OpenStruct.new(wish: wish, color: color)
-    end
   end
 
   def create
     @wish = Wish.new(wish_params)
     if @wish.save
-      redirect_to root_path, notice: "願いごとを投稿しました"
+      redirect_to root_path, success: "あなたの願いが叶いますように"
     else
-      @wishes = Wish.order(created_at: :desc)
       render :index, status: :unprocessable_entity
     end
   end
@@ -21,5 +18,9 @@ class WishesController < ApplicationController
 
   def wish_params
     params.require(:wish).permit(:name, :message)
+  end
+
+  def set_wishes
+    @wishes = Wish.order(created_at: :desc)
   end
 end
